d06497af48abfc6401132a687103ea033710554f
Trishane Reid <trishanereid17@gmail.com>
2024-11-24 13:50:22 +0530

    did minor implementations on betting history

--- lib/betting_history_screen.dart
+++ lib/betting_history_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class BettingHistoryScreen extends StatelessWidget {
+
+  DateTime selectedDate = DateTime.now();
+
   final List<Map<String, dynamic>> historyData = [
     {
       'round': 1945,
@@ -120,92 +124,157 @@ class BettingHistoryScreen extends StatelessWidget {
     },
   ];
 
+  String getFormattedDate(DateTime date) {
+    return DateFormat('MMM, dd yyyy').format(date);
+  }
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.red.shade700,
-        title: const Text('My Record'),
+        title: const Text(
+          'My Records',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            color: Colors.white,
+          ),
+        ),
         actions: [
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Row(
-              children: [
-                const Text(
-                  'Nov,18 2024',
-                  style: TextStyle(fontSize: 16),
+            child: StatefulBuilder(
+              builder: (context, setState) => GestureDetector(
+                onTap: () async {
+                  final DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: selectedDate,
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2100),
+                  );
+                  if (pickedDate != null && pickedDate != selectedDate) {
+                    setState(() {
+                      selectedDate = pickedDate;
+                    });
+                  }
+                },
+                child: Row(
+                  children: [
+                    Text(
+                      getFormattedDate(selectedDate),
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    const Icon(Icons.arrow_drop_down, color: Colors.white,),
+                  ],
                 ),
-                const Icon(Icons.arrow_drop_down),
-              ],
+              ),
             ),
           ),
         ],
-      ),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(8),
-        itemCount: historyData.length,
-        itemBuilder: (context, index) {
-          final data = historyData[index];
-          return Card(
-            margin: const EdgeInsets.symmetric(vertical: 8),
-            color: Colors.red.shade800,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFF2B73F4),
+                Color(0xFF1A75D1)
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
             ),
-            child: ListTile(
-              title: Text(
-                'Round: ${data['round']}',
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Play: ${data['currency'] == 'gold' ? '🪙' : '💎'} ${data['playAmount']}',
-                    style: const TextStyle(
-                      color: Colors.white70,
-                      fontSize: 16,
-                    ),
-                  ),
-                  Text(
-                    data['time'],
-                    style: const TextStyle(
-                      color: Colors.white70,
-                      fontSize: 14,
-                    ),
-                  ),
+          ),
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.blue.shade700,
+              Colors.indigo.shade600,
+              Colors.blueAccent.shade400,
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+
+        child: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: historyData.length,
+          itemBuilder: (context, index) {
+            final data = historyData[index];
+            return Container(
+              margin: const EdgeInsets.symmetric(vertical: 8),
+              decoration: BoxDecoration(
+                gradient: RadialGradient( colors: [
+                  Colors.blue.shade400,
+                  Colors.blue.shade800,
                 ],
+                    radius: 4
+                ),
+                borderRadius: BorderRadius.circular(12),
               ),
-              trailing: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    data['status'],
-                    style: TextStyle(
-                      color: data['status'] == 'Missed' ? Colors.white : Colors.yellow,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
+              child: ListTile(
+                title: Text(
+                  'Round: ${data['round']}',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
                   ),
-                  if (data['reward'] != null)
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
                     Text(
-                      data['reward'],
+                      'Play: ${data['currency'] == 'gold' ? '🪙' : '💎'} ${data['playAmount']}',
                       style: const TextStyle(
-                        color: Colors.yellow,
+                        color: Colors.white70,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      data['time'],
+                      style: const TextStyle(
+                        color: Colors.white70,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+                trailing: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      data['status'],
+                      style: TextStyle(
+                        color: data['status'] == 'Missed' ? Colors.deepOrangeAccent : Colors.yellow,
                         fontWeight: FontWeight.bold,
                         fontSize: 16,
                       ),
                     ),
-                ],
+                    if (data['reward'] != null)
+                      Text(
+                        data['reward'],
+                        style: const TextStyle(
+                          color: Colors.yellow,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                  ],
+                ),
               ),
-            ),
-          );
-        },
-      ),
+            );
+          },
+        ),
+      )
     );
   }
 }
